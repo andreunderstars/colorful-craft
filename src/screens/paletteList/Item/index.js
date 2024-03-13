9ed04d494af70e26c2dc9200a323aa6e49c80058
Andre <meloluizandre10@gmail.com>
2024-03-12 23:47:57 -0300

    Create Colors component for code refacturing

--- src/screens/paletteList/Item/index.js
+++ src/screens/paletteList/Item/index.js
@@ -6,15 +6,16 @@ import {
   View,
   FlatList,
 } from "react-native";
+import Colors from "./Colors";
 
 export default function Item({ name, author, colors }) {
-  const oversized = colors.length > 32;
-  const firstHalf = colors.slice(0, colors.length / 3);
-  const secondHalf = colors.slice(
+  const oversized = colors.length > 24;
+  const firstLine = colors.slice(0, colors.length / 3);
+  const secondLine = colors.slice(
     colors.length / 3 + 1,
     (2 * colors.length) / 3
   );
-  const thirdHalf = colors.slice((2 * colors.length) / 3 + 1, colors.length);
+  const thirdLine = colors.slice((2 * colors.length) / 3 + 1, colors.length);
   return (
     <>
       <TouchableOpacity style={styles.container}>
@@ -24,66 +25,10 @@ export default function Item({ name, author, colors }) {
         </View>
         <View style={styles.rectangle}>
           <View style={styles.colorDisplay}>
-            {!oversized && (
-              <FlatList
-                style={styles.colorRow}
-                data={colors}
-                renderItem={(item) => (
-                  <View
-                    style={{
-                      flex: 1,
-                      backgroundColor: item.item,
-                      width: 330 / colors.length,
-                    }}
-                  ></View>
-                )}
-              />
-            )}
-            {oversized && (
-              <FlatList
-                style={styles.colorRow}
-                data={firstHalf}
-                renderItem={(item) => (
-                  <View
-                    style={{
-                      flex: 1,
-                      backgroundColor: item.item,
-                      width: 330 / firstHalf.length,
-                    }}
-                  ></View>
-                )}
-              />
-            )}
-            {oversized && (
-              <FlatList
-                style={styles.colorRow}
-                data={secondHalf}
-                renderItem={(item) => (
-                  <View
-                    style={{
-                      flex: 1,
-                      backgroundColor: item.item,
-                      width: 330 / secondHalf.length,
-                    }}
-                  ></View>
-                )}
-              />
-            )}
-            {oversized && (
-              <FlatList
-                style={styles.colorRow}
-                data={thirdHalf}
-                renderItem={(item) => (
-                  <View
-                    style={{
-                      flex: 1,
-                      backgroundColor: item.item,
-                      width: 330 / thirdHalf.length,
-                    }}
-                  ></View>
-                )}
-              />
-            )}
+            {!oversized && <Colors colors={colors} />}
+            {oversized && <Colors colors={firstLine} />}
+            {oversized && <Colors colors={secondLine} />}
+            {oversized && <Colors colors={thirdLine} />}
           </View>
         </View>
       </TouchableOpacity>
@@ -115,13 +60,5 @@ const styles = StyleSheet.create({
   colorDisplay: {
     flex: 1,
     padding: 3,
-  },
-  colorRow: {
-    flex: 1,
-    flexDirection: "row",
-  },
-  color: {
-    width: 50,
-    height: 85,
   },
 });
