903c580fbca95bafced927832bd8e1a51e794887
Andre <meloluizandre10@gmail.com>
2024-03-12 23:37:03 -0300

    Add color palettes to FlatList

--- src/screens/paletteList/Item/index.js
+++ src/screens/paletteList/Item/index.js
@@ -1,7 +1,20 @@
 import React from "react";
-import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+} from "react-native";
 
-export default function Item({ name, author }) {
+export default function Item({ name, author, colors }) {
+  const oversized = colors.length > 32;
+  const firstHalf = colors.slice(0, colors.length / 3);
+  const secondHalf = colors.slice(
+    colors.length / 3 + 1,
+    (2 * colors.length) / 3
+  );
+  const thirdHalf = colors.slice((2 * colors.length) / 3 + 1, colors.length);
   return (
     <>
       <TouchableOpacity style={styles.container}>
@@ -9,7 +22,70 @@ export default function Item({ name, author }) {
           <Text style={styles.name}>{name.toUpperCase()}</Text>
           <Text style={styles.user}>{author}</Text>
         </View>
-        <View style={styles.rectangle}></View>
+        <View style={styles.rectangle}>
+          <View style={styles.colorDisplay}>
+            {!oversized && (
+              <FlatList
+                style={styles.colorRow}
+                data={colors}
+                renderItem={(item) => (
+                  <View
+                    style={{
+                      flex: 1,
+                      backgroundColor: item.item,
+                      width: 330 / colors.length,
+                    }}
+                  ></View>
+                )}
+              />
+            )}
+            {oversized && (
+              <FlatList
+                style={styles.colorRow}
+                data={firstHalf}
+                renderItem={(item) => (
+                  <View
+                    style={{
+                      flex: 1,
+                      backgroundColor: item.item,
+                      width: 330 / firstHalf.length,
+                    }}
+                  ></View>
+                )}
+              />
+            )}
+            {oversized && (
+              <FlatList
+                style={styles.colorRow}
+                data={secondHalf}
+                renderItem={(item) => (
+                  <View
+                    style={{
+                      flex: 1,
+                      backgroundColor: item.item,
+                      width: 330 / secondHalf.length,
+                    }}
+                  ></View>
+                )}
+              />
+            )}
+            {oversized && (
+              <FlatList
+                style={styles.colorRow}
+                data={thirdHalf}
+                renderItem={(item) => (
+                  <View
+                    style={{
+                      flex: 1,
+                      backgroundColor: item.item,
+                      width: 330 / thirdHalf.length,
+                    }}
+                  ></View>
+                )}
+              />
+            )}
+          </View>
+        </View>
       </TouchableOpacity>
     </>
   );
@@ -31,8 +107,21 @@ const styles = StyleSheet.create({
   name: { fontWeight: "bold", fontSize: 24 },
   user: { fontSize: 15 },
   rectangle: {
-    backgroundColor: "#BC9CC7",
+    flex: 1,
+    backgroundColor: "#000000",
     height: 90,
     width: 336,
+  },
+  colorDisplay: {
+    flex: 1,
+    padding: 3,
+  },
+  colorRow: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  color: {
+    width: 50,
+    height: 85,
   },
 });
--- src/screens/paletteList/index.js
+++ src/screens/paletteList/index.js
@@ -30,7 +30,88 @@ const palettes = [
     id: 2,
     name: "miku",
     author: "user1234",
-    colors: ["1e014b", "78478e", "58e1c3", "ffdaff"],
+    colors: ["#1e014b", "#78478e", "#58e1c3", "#ffdaff"],
+  },
+  {
+    id: 3,
+    name: "light & dark",
+    author: "Luis Miguel Maldonado",
+    colors: [
+      "#0d2b45",
+      "#203c56",
+      "#544e68",
+      "#8d697a",
+      "#d08159",
+      "#ffaa5e",
+      "#ffd4a3",
+      "#ffecd6",
+    ],
+  },
+  {
+    id: 4,
+    name: "sage57",
+    author: "strawbrysage",
+    colors: [
+      "#040519",
+      "#432547",
+      "#604c7d",
+      "#908cc7",
+      "#b0cfee",
+      "#ffffff",
+      "#f5b554",
+      "#d58141",
+      "#ac4c3f",
+      "#732c40",
+      "#4e1a34",
+      "#6c0c5e",
+      "#9d1952",
+      "#f36150",
+      "#ef8277",
+      "#ffae71",
+      "#fff2c4",
+      "#fabc94",
+      "#fd8e82",
+      "#ff5b5f",
+      "#ea2a53",
+      "#b90e4a",
+      "#850950",
+      "#4b0843",
+      "#e82337",
+      "#fa6735",
+      "#ff9d3e",
+      "#fff57a",
+      "#8de35d",
+      "#2cc94e",
+      "#0b9d5f",
+      "#0a6e65",
+      "#10325b",
+      "#0b1646",
+      "#164a67",
+      "#237c8a",
+      "#3db2ad",
+      "#5be5c0",
+      "#b8fff0",
+      "#5adbfa",
+      "#4c89ee",
+      "#4837c7",
+      "#3d1b83",
+      "#370950",
+      "#691d8c",
+      "#9e2cab",
+      "#d747bc",
+      "#ff74be",
+      "#ffc1d4",
+      "#ff7eac",
+      "#d75192",
+      "#a4318e",
+      "#6b1f7a",
+    ],
+  },
+  {
+    id: 5,
+    name: "twilight 5",
+    author: "Star",
+    colors: ["#fbbbad", "#ee8695", "#4a7a96", "#333f58", "#292831"],
   },
 ];
 
@@ -43,7 +124,11 @@ export default function PaletteList() {
         <FlatList
           data={palettes}
           renderItem={(item) => (
-            <Item name={item.item.name} author={item.item.author} />
+            <Item
+              name={item.item.name}
+              author={item.item.author}
+              colors={item.item.colors}
+            />
           )}
           keyExtractor={({ id }) => String(id)}
         />
