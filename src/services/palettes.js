b760aa9f4a71fe8c9c55dd5b20eee1e794d9fd8f
Andre <meloluizandre10@gmail.com>
2024-03-25 17:04:39 -0300

    Add Edit and Delete buttons for palettes

--- src/services/palettes.js
+++ src/services/palettes.js
@@ -14,3 +14,28 @@ export async function createPalette(name, author, colors) {
     return "error";
   }
 }
+
+export async function editPalette(id, name, author, colors) {
+  try {
+    await api.put(`/palettes/${id}`, {
+      id: id,
+      name: name,
+      author: author,
+      colors: colors,
+    });
+    return "success";
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
+}
+
+export async function deletePalette(id) {
+  try {
+    await api.delete(`/palettes/${id}`);
+    return "success";
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
+}
